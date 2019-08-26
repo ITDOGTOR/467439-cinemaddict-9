a7c397e43978664b51325b3f1e8d6cedebe0e178
Роман <romkich16@gmail.com>
2019-08-27 00:02:29 +0300

    Изменил функцию вывода нескольких элементов из массива

--- src/util.js
+++ src/util.js
@@ -1,20 +1,6 @@
 const getRandomInt = ({min, max}) => Math.floor(Math.random() * (max - min + 1)) + min;
 const getRandomArrayElement = (array) => array[Math.floor((Math.random() * array.length) + 0)];
-const getRandomArrayElements = (array, elementCount) => {
-  const copyArray = array.slice();
-  const copyArrayLength = array.length >= elementCount ? array.length - elementCount : 0;
-
-  if (array.length >= elementCount) {
-    while (--array.length > copyArrayLength) {
-      let index = Math.floor((array.length) * Math.random());
-      let temp = copyArray[index];
-      copyArray[index] = copyArray[array.length];
-      copyArray[array.length] = temp;
-    }
-  }
-
-  return copyArray.slice(copyArrayLength);
-};
+const getRandomArrayElements = (array, elementCount) => array.slice().sort(() => Math.random() - 0.5).slice(0, elementCount);
 const getRandomBoolean = () => Boolean(Math.random() > 0.5);
 
 export {getRandomInt, getRandomArrayElement, getRandomArrayElements, getRandomBoolean};
