aa39444c626caf64246b1f48fbdb2c18d8ad1f24
Роман <romkich16@gmail.com>
2019-08-29 17:18:09 +0300

    Добавил вспомогательные функции отрисовки

--- src/util.js
+++ src/util.js
@@ -1,6 +1,34 @@
+const Position = {
+  AFTERBEGIN: `afterbegin`,
+  BEFOREEND: `beforeend`
+};
+
+const createElement = (template) => {
+  const newElement = document.createElement(`div`);
+  newElement.innerHTML = template;
+  return newElement.firstChild;
+};
+
+const renderElement = (container, element, place = Position.BEFOREEND) => {
+  switch (place) {
+    case Position.AFTERBEGIN:
+      container.prepend(element);
+      break;
+    case Position.BEFOREEND:
+      container.append(element);
+      break;
+  }
+};
+
+const unrenderElement = (element) => {
+  if (element) {
+    element.remove();
+  }
+};
+
 const getRandomInt = ({min, max}) => Math.floor(Math.random() * (max - min + 1)) + min;
 const getRandomArrayElement = (array) => array[Math.floor((Math.random() * array.length) + 0)];
 const getRandomArrayElements = (array, elementCount) => array.slice().sort(() => Math.random() - 0.5).slice(0, elementCount);
 const getRandomBoolean = () => Boolean(Math.random() > 0.5);
 
-export {getRandomInt, getRandomArrayElement, getRandomArrayElements, getRandomBoolean};
+export {getRandomInt, getRandomArrayElement, getRandomArrayElements, getRandomBoolean, createElement, renderElement, unrenderElement, Position};
